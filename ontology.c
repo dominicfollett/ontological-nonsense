33688aa8933ee2bc05c7feaf56b8b3fda4a347ff
dominicfollett <dominic.follett.smith@gmail.com>
2016-08-03 18:35:13 +0200

    Try out stallman formatting

--- ontology.c
+++ ontology.c
@@ -5,93 +5,107 @@
 #include "question.h"
 
 /* Prototype declaration */
-int get_line(char * workload_path);
+int get_line(char *workload_path);
 
-FILE * file_handle = NULL;
-char * line = NULL;
-size_t len  = 0;
+FILE *file_handle = NULL;
+char *line = NULL;
+size_t len = 0;
 
-int get_line(char *workload_path){
-  if (!file_handle) {
-    file_handle = fopen(workload_path, "r");
-    if (!file_handle) {
-      fprintf(stderr,"cannot open workload file %s\n", workload_path);
-      exit(EXIT_FAILURE);
-    }
-  }
-  if(getline(&line, &len, file_handle) != -1){
-    return 0;
-  }else{
-    free(line);
-    fclose(file_handle);
-    return -1;
-  }
+int get_line(char *workload_path)
+{
+	if (!file_handle)
+	{
+		file_handle = fopen(workload_path, "r");
+		if (!file_handle)
+		{
+			fprintf(stderr, "cannot open workload file %s\n", workload_path);
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (getline(&line, &len, file_handle) != -1)
+	{
+		return 0;
+	}
+	else
+	{
+		free(line);
+		fclose(file_handle);
+		return -1;
+	}
 }
 
-int main(int argc, char* argv[]){
-  char * ontology;
-  int m_questions, k_queries, i, score;
-  struct sub_topics_list * list;
-  struct question ** questions_hash;
-  struct sub_topics_list * tmp_list;
-  char * token;
-  char * tmp_str;
+int main(int argc, char *argv[])
+{
+	char *ontology;
+	int m_questions, k_queries, i, score;
+	struct sub_topics_list *list;
+	struct question **questions_hash;
+	struct sub_topics_list *tmp_list;
+	char *token;
+	char *tmp_str;
 
-  if (argv[argc - 1] == NULL){
-    fprintf(stderr,"No work load file specified\n");
-    exit(EXIT_FAILURE);
-  }
+	if (argv[argc - 1] == NULL)
+	{
+		fprintf(stderr, "No work load file specified\n");
+		exit(EXIT_FAILURE);
+	}
 
-  list = parser_init();
-  get_line(argv[1]);
+	list = parser_init();
+	get_line(argv[1]);
 
-  get_line(NULL);
-  copy_string(line, &ontology);
+	get_line(NULL);
+	copy_string(line, &ontology);
 
-  get_line(NULL);
-  m_questions = atoi(line);
+	get_line(NULL);
+	m_questions = atoi(line);
 
-  questions_hash = question_init(m_questions);
-  i = m_questions;
+	questions_hash = question_init(m_questions);
+	i = m_questions;
 
-  /* Insert questions */
-  while(i > 0) {
-    get_line(NULL);
-    token = strtok(line, ":");
-    question_insert(&questions_hash, token, m_questions, strtok(NULL, ":"));
-    i--;
-  }
+	/* Insert questions */
+	while (i > 0)
+	{
+		get_line(NULL);
+		token = strtok(line, ":");
+		question_insert(&questions_hash, token, m_questions,
+						strtok(NULL, ":"));
+		i--;
+	}
 
-  get_line(NULL);
-  k_queries = atoi(line);
+	get_line(NULL);
+	k_queries = atoi(line);
 
-  while(k_queries > 0 ) {
-    get_line(NULL);
-    tmp_str = (char *) malloc(strlen(line) +1);
-    strcpy(tmp_str, line);
-    token = strtok(tmp_str, " ");
+	while (k_queries > 0)
+	{
+		get_line(NULL);
+		tmp_str = (char *)malloc(strlen(line) + 1);
+		strcpy(tmp_str, line);
+		token = strtok(tmp_str, " ");
 
-    parser_get_sub_topics_list(&list, ontology, token);
-    line+=strlen(token) +1;
+		parser_get_sub_topics_list(&list, ontology, token);
+		line += strlen(token) + 1;
 
-    tmp_list = list;
+		tmp_list = list;
 
-    score = 0;
-    while (tmp_list->topic_parent) {
-      score += question_count(&questions_hash, m_questions, line, tmp_list->topic);
-      tmp_list = tmp_list->topic_parent;
-    }
+		score = 0;
+		while (tmp_list->topic_parent)
+		{
+			score +=
+				question_count(&questions_hash, m_questions, line,
+							   tmp_list->topic);
+			tmp_list = tmp_list->topic_parent;
+		}
 
-    printf("%i\n",score);
-    line -= strlen(token) +1;
-    free(tmp_str);
-    k_queries--;
-  }
+		printf("%i\n", score);
+		line -= strlen(token) + 1;
+		free(tmp_str);
+		k_queries--;
+	}
 
-  free(line);
-  parser_destroy(&list);
-  question_destroy(&questions_hash, m_questions);
-  free(ontology);
-  fclose(file_handle);
-  return 0;
+	free(line);
+	parser_destroy(&list);
+	question_destroy(&questions_hash, m_questions);
+	free(ontology);
+	fclose(file_handle);
+	return 0;
 }
