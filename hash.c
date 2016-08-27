d31e792e11a89cfedc18c3f4edf9c302150426fa
dominicfollett <dominic.follett.smith@gmail.com>
2016-08-27 08:57:28 +0200

    Make Hashmap support multiple different types

--- hash.c
+++ hash.c
@@ -3,9 +3,24 @@
 #include <string.h>
 #include "hash.h"
 
+struct hash_s { /* Just always use this as our type */
+  void * data;
+};
 
-void ** hash_init(int struct_pointer_size, int number_of_elements) {
-  return (void **) malloc(struct_pointer_size*number_of_elements);
+hash_map hash_init(int struct_pointer_size, int number_of_elements) {
+  /* Assume worst case scenario where the matrix is number of elements^2 */
+  /* Don't really have to resize the hash_map/matix ... */
+  int size;
+  struct hash_s ** hash_map;
+
+  size = (sizeof(struct hash_s *))*number_of_elements;
+  hash_map = malloc(size);
+
+  while(number_of_elements--)
+  {
+    hash_map[number_of_elements] = malloc(size);
+  }
+  return hash_map;
 }
 
 static unsigned long hash(char *str, unsigned long modulo)
@@ -17,4 +32,16 @@ static unsigned long hash(char *str, unsigned long modulo)
 		hash = ((hash << 5) + hash) + c;	/* hash * 33 + c */
 
 	return hash % modulo;
+}
+
+void hash_insert(hash_map map, void * pointer, char * key, int size)
+{
+  unsigned long index;
+  struct hash_s * iterator;
+
+  index = hash(key, size);
+  iterator = map[index];
+
+  while(iterator) iterator++;
+  iterator->data = pointer;
 }
--- hash.h
+++ hash.h
@@ -2,13 +2,14 @@
  * Hash implementation for reuse accross the project.
  */
 
-typedef void ** hash_map;
+typedef struct hash_s ** _hash_map;
+typedef _hash_map hash_map;
 
 /*
  * Initialize the hashmap
  * Int is acceptible since the ontology size is small << 2^20
  */
-void ** hash_init(int struct_pointer_size, int number_of_elements);
+hash_map hash_init(int struct_pointer_size, int number_of_elements);
 
 /*
  * Compute a hash from a given string and modulo (hash array size)
@@ -18,6 +19,7 @@ static unsigned long hash(char *str, unsigned long modulo);
 /*
  * Insert into the hash_map, with separate chaining for minimize collisions.
  */
+void hash_insert(hash_map map, void * pointer, char * key, int size);
 
 /*
  * A cleanup method, we don't cleanup whatever is being pointed to.
